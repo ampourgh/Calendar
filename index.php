ff8df17ca260d0f870bee91cd96d4a9276ea78f6
ampourgh <ampourgh@gmail.com>
2018-03-27 14:20:46 -0500

    feat: top banner and extra checkboxes
    The month and day were places into an HTML and CSS banner, 6 extra checkboxes were added for the assigned tasks.

--- index.php
+++ index.php
@@ -13,12 +13,21 @@
 
       // Day of the week and day of the month
       // Month and year
-      echo date("l jS ") . "<br>";
-      echo date("F Y") . "<br>";
+
+      echo '<div class="month">'
+        . '<ul>'
+        . '<li class="prev">&#10094;</li>'
+        .  '<li class="next">&#10095;</li>'
+        .  '<li>' . date("F Y")
+        .  '<br>'
+        .  '<span style="font-size:18px">'
+        . date("l jS ") . "<br>" . '</span></li>'
+        . '</ul>'
+        . '</div>';
 
       // Lists the days within the week
 
-      $daysArray = array('Mon', 'Tue', 'Wen', 'Thu', 'Fri', 'Sat', 'Su');
+      $daysArray = array('Mon', 'Tue', 'Wen', 'Thu', 'Fri', 'Sat', 'Sun');
 
       echo '<ul class="weekdays">';
       foreach($daysArray as $day) {
@@ -32,26 +41,52 @@
       $firstDay = date('D', $timestamp);
 
       // Lists the days within the current month
+      echo '<form action="" method="POST">';
+        echo '<ul class="days">';
+        $j = 0;
+        for ($i=1; $i < (cal_days_in_month(CAL_GREGORIAN, date("m"), date("Y")) + 1); $i++) {
+          if($firstDay != $daysArray[$j]) {
+            echo '<li></li>';
+            $i--;
+            $j++;
 
-      echo '<ul class="days">';
-      $j = 0;
-      for ($i=1; $i < (cal_days_in_month(CAL_GREGORIAN, date("m"), date("Y")) + 1); $i++) {
-        if($firstDay != $daysArray[$j]) {
-          echo '<li><input id="checkBox" type="checkbox"></li>';
-          $i--;
-          $j++;
-
-        }
-        else {
-          if($i == date("j")) {
-            echo '<li><span class="active">' . $i . '<input id="checkBox" type="checkbox"></span></li>';
           }
           else {
-            echo '<li>' . $i . '<input id="checkBox" type="checkbox"></li>';
+            if($i == date("j")) {
+              echo '<li><span class="active">'
+              . $i. '</span>' . '<br>'
+              . '<input id="checkBox" type="checkbox">' . '<br>'
+              . '<input id="checkBox" type="checkbox">' . '<br>'
+              . '<input id="checkBox" type="checkbox">' . '<br>'
+              . '<input id="checkBox" type="checkbox">' . '<br>'
+              . '<input id="checkBox" type="checkbox">' . '<br>'
+              . '<input id="checkBox" type="checkbox">' . '<br>'
+              . '<input id="checkBox" type="checkbox">' . '<br>'
+              . '</li>';
+            }
+            else {
+              echo '<li>'
+                    . $i . '<br>'
+                    . '<input id="checkBox" type="checkbox">' . '<br>'
+                    . '<input id="checkBox" type="checkbox">' . '<br>'
+                    . '<input id="checkBox" type="checkbox">' . '<br>'
+                    . '<input id="checkBox" type="checkbox">' . '<br>'
+                    . '<input id="checkBox" type="checkbox">' . '<br>'
+                    . '<input id="checkBox" type="checkbox">' . '<br>'
+                    . '<input id="checkBox" type="checkbox">' . '<br>'
+                    .'</li>';
+            }
           }
         }
+        echo '</ul>';
+        echo '<input type="submit" value="Submit">';
+      echo '</form>';
+
+      if(isset($_POST['click']))
+      {
+          $date_clicked = date('Y-m-d H:i:s');;
+          echo "Time the button was clicked: " . $date_clicked . "<br>";
       }
-      echo '</ul>';
     ?>
   </body>
 </body>
